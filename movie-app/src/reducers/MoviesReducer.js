703dc6abdf32af07d464e0dee0892a4c3d93f00c
Hema <hema.passi@gmail.com>
2020-09-01 10:46:26 +0530

    'data'

--- movie-app/src/reducers/MoviesReducer.js
+++
@@ -1,8 +0,0 @@
-import { FETCH_MOVIES } from "../actions/index";
-export default function (state = [], action) {
-  switch (action.type) {
-    case FETCH_MOVIES:
-      return [action.payload.data];
-  }
-  return state;
-}
